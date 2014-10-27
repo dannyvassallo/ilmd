e1b1197fcb315dff7c77804008bb3fd61a065073
Dan Vassallo <danielvassallo87@gmail.com>
2014-10-27 15:14:23 -0400

    Pagination Works Yo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @query = Micropost.search do
       fulltext params[:search]
+      paginate :page => params[:page] || 1, :per_page => 10
       order_by :id, :desc
     end
     @microposts = @query.results
