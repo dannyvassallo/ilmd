7917e7849537d4320434c78df40101c0bb6123b5
Dan Vassallo <danielvassallo87@gmail.com>
2014-10-27 14:32:55 -0400

    Email notifications are a go

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -30,6 +30,7 @@ class MicropostsController < ApplicationController
       if @micropost.save
         format.html { redirect_to @micropost, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @micropost }
+        PostMailer.posted_confirmation(@micropost.user).deliver
       else
         format.html { render :new }
         format.json { render json: @micropost.errors, status: :unprocessable_entity }
