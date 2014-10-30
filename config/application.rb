aa1f89ffcebef96f795efef7f8f948bcecfedca3
Dan Vassallo <danielvassallo87@gmail.com>
2014-10-30 10:58:16 -0400

    initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Ilmd
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.secret_key_base = Figaro.env.secret_key_base
+    config.assets.initialize_on_precompile = false
   end
 end
 
