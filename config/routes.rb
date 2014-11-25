667655b5dfe61e101bf6688ef25a5f04fe317d45
Dan Vassallo <danielvassallo87@gmail.com>
2014-11-25 10:51:02 -0500

    Added CSV upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :microposts
+  resources :microposts do
+    collection { post :import}
+  end
 
   root to: 'home#index'
   get 'status' => 'status#index'
